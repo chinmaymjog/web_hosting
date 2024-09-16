b8dc73c28cfccd1607bd78d4d035fe53cdb211d4
Chinmay Jog <chinmaymjog@gmail.com>
2024-09-16 18:43:32 +0530

    Added nsg rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,20 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+resource "azurerm_network_security_rule" "web" {
+  name                        = "WebAccess"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80,443"
+  source_address_prefix       = "*"
+  destination_address_prefixs  = var.snet_web
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.p_short}-${var.e_short}-${var.l_short}"
   location            = var.location
